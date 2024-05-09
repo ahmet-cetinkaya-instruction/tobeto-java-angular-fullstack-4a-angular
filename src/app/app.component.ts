449617cdbee8ae37e7d25e3d399f7ff9c69d646c
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-05-09 13:53:00 +0300

    remove unused imports and code from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { CommonModule } from '@angular/common';
 
 // Components: Angular tarafında görünüm ve işlev bakımından birer küçük parçacıklardır.
 @Component({
@@ -14,31 +11,10 @@ import { CommonModule } from '@angular/common';
   // Her component'in kendi import'ı olabilir.
   // Böylece her component kendi başına Angulara dahil olabilir.
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent],
+  imports: [],
   // Import'larda bu componentte kullanmak adına Angular modüllerini sağlamış oluyoruz.
 
   templateUrl: './app.component.html', // HTML dosyası yolunu
   styleUrl: './app.component.scss', // Stil dosya yolunu
 })
-export class AppComponent {
-  // Component'in class'ıyla dinamik yapıyı, temel işlev ve kararları yapabiliriz.
-  readonly title: string = 'Northwind'; // State: Component'in sakladığı veri ve izlediği veri yapısı
-  counter: number = 0;
-
-  products: { name: string; price: number; discontinued: boolean }[] = [
-    { name: 'Çay', price: 100, discontinued: false },
-    { name: 'Kahve', price: 200, discontinued: true },
-    { name: 'Su', price: 10, discontinued: false },
-    { name: 'Çikolata', price: 50, discontinued: true },
-    { name: 'Kek', price: 150, discontinued: false },
-    { name: 'Bisküvi', price: 75, discontinued: true },
-  ];
-  get filteredProducts() {
-    return this.products.filter((p) => !p.discontinued);
-  }
-
-  onButtonClick() {
-    this.counter += 1; // State'i güncelleyen fonksiyon
-    console.log('Button Clicked');
-  }
-}
+export class AppComponent {}
