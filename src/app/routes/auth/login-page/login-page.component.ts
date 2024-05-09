5984967698ceb4a31dbdbffeacd9d1d246aa3f44
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-05-09 16:00:09 +0300

    add layout components

--- src/app/routes/auth/login-page/login-page.component.ts
+++ src/app/routes/auth/login-page/login-page.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BasicLayoutComponent } from '../../../shared/components/basic-layout/basic-layout.component';
 
 @Component({
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+    BasicLayoutComponent
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
--- src/app/routes/home-page/home-page.component.ts
+++ src/app/routes/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BasicLayoutComponent } from '../../shared/components/basic-layout/basic-layout.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, BasicLayoutComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
