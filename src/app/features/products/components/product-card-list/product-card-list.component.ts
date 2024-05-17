9f6378245d968974df41a3a827e4badf301ce003
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-05-17 10:41:07 +0300

    add VatPipe for calculating VAT on product prices

--- src/app/features/products/components/product-card-list/product-card-list.component.ts
+++ src/app/features/products/components/product-card-list/product-card-list.component.ts
@@ -13,11 +13,12 @@ import {
 import { ProductListItem } from '../../models/product-list-item';
 import { CardComponent } from '../../../../shared/components/card/card.component';
 import { ProductsService } from '../../services/products.service';
+import { VatPipe } from '../../pipes/vat.pipe';
 
 @Component({
   selector: 'app-product-card-list',
   standalone: true,
-  imports: [CommonModule, CardComponent],
+  imports: [CommonModule, CardComponent, VatPipe],
   templateUrl: './product-card-list.component.html',
   styleUrl: './product-card-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
