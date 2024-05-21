ce95c138af63761b095002e80bc00d6d6f13b5df
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-05-21 14:01:23 +0300

    add pagination to products service

--- src/app/features/products/services/products.service.ts
+++ src/app/features/products/services/products.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable, Subject } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { ProductListItem } from '../models/product-list-item';
 import { ProductDetails } from '../models/product-details';
 import { environment } from '../../../../environments/environment';
+import { PaginatedList } from '../../../core/models/paginated-list';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +14,11 @@ export class ProductsService {
 
   constructor(private http: HttpClient) {}
 
-  getList(filterByCategoryId: number | null): Observable<ProductListItem[]> {
+  getList(
+    pageIndex: number,
+    pageSize: number,
+    filterByCategoryId: number | null
+  ): Observable<PaginatedList<ProductListItem>> {
     let queryParams: any = {};
     if (filterByCategoryId) {
       queryParams.categoryId = filterByCategoryId.toString();
@@ -25,38 +30,54 @@ export class ProductsService {
       })
       .pipe(
         // pipe: Bir veya daha fazla operatörü birbirine bağlar. Böylece Observable yapıların davranışını değiştirilebilir. https://rxjs.dev/api/operators/
+        // map: Bir veri akıştan başka bir veri akışa dönüşüm yapar. https://rxjs.dev/api/operators/map
 
+        // Pagination
+        map((response) => {
+          // Geçici olarak FE tarafında gerçeleştirdik. Daha sonra BE tarafında, veri tabanı üzerinden gerçekleştirilecek.
+          const paginatedList: PaginatedList<ProductListItem> = {
+            items: response.slice(
+              pageIndex * pageSize,
+              pageIndex * pageSize + pageSize
+            ),
+            pageIndex,
+            pageSize,
+            totalCount: response.length,
+          };
+          return paginatedList;
+        }),
+
+        // Image Url
         map((data) => {
-          for (const product of data) {
+          for (const product of data.items) {
             product.imageUrl = 'https://via.placeholder.com/500';
           }
           return data;
-          // map: Bir veri akıştan başka bir veri akışa dönüşüm yapar. https://rxjs.dev/api/operators/map
         })
       );
 
-    const subject = new Subject<ProductListItem[]>();
-    // Subject: Veri akışını olduğunda, hata olduğunda ve tamamlandığında gözlemleyenlere haber veren bir yapıdır.
-    // BehaviorSubject: Subject'in bir türüdür. Ancak, bir BehaviorSubject, abone olmadan önceki en son değeri saklar ve yeni abonelere bu değeri hemen gönderir.
+    // const subject = new Subject<ProductListItem[]>();
+    // // Subject: Veri akışını olduğunda, hata olduğunda ve tamamlandığında gözlemleyenlere haber veren bir yapıdır.
+    // // BehaviorSubject: Subject'in bir türüdür. Ancak, bir BehaviorSubject, abone olmadan önceki en son değeri saklar ve yeni abonelere bu değeri hemen gönderir.
 
-    this.http.get<ProductListItem[]>(this.apiControllerUrl).subscribe({
-      next: (response) => {
-        for (const product of response) {
-          product.imageUrl = 'https://via.placeholder.com/500';
-        }
-        console.log('ProductService:', response);
+    // this.http.get<ProductListItem[]>(this.apiControllerUrl).subscribe({
+    //   next: (response) => {
+    //     for (const product of response) {
+    //       product.imageUrl = 'https://via.placeholder.com/500';
+    //     }
+    //     console.log('ProductService:', response);
 
-        subject.next(response);
-      },
-      error: (error) => {
-        subject.error(error);
-      },
-      complete: () => {
-        subject.complete();
-      },
-    });
+    //     subject.next(response);
+    //   },
+    //   error: (error) => {
+    //     subject.error(error);
+    //   },
+    //   complete: () => {
+    //     subject.complete();
+    //   },
+    // });
 
-    return subject.asObservable();
+    // return subject.asObservable();
   }
 
   getById(id: number): Observable<ProductDetails> {
