631efd9dfce54cedfa64589dc0f6fc35e557a9f4
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-05-23 15:15:19 +0300

    refactor: update AUTH_ERROR_REDIRECT_URL_TOKEN injection token

--- src/app/core/auth/providers/auth.provider.ts
+++ src/app/core/auth/providers/auth.provider.ts
@@ -1,8 +1,7 @@
 import { InjectionToken, Provider } from '@angular/core';
 
-const AUTH_ERROR_REDIRECT_URL = 'AUTH_ERROR_REDIRECT_URL';
 export const AUTH_ERROR_REDIRECT_URL_TOKEN = new InjectionToken<string>(
-  AUTH_ERROR_REDIRECT_URL
+  'AUTH_ERROR_REDIRECT_URL'
 );
 
 export function provideAuthErrorRedirectUrl(url: string): Provider {
