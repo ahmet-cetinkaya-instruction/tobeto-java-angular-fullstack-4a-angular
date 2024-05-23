13e2cad5521759956ea56b2b62875a0b72890d22
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2024-05-23 14:13:31 +0300

    add examples pages

--- src/app/routes/auth/auth.routes.ts
+++ src/app/routes/auth/auth.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { MyAccountPageComponent } from './my-account-page/my-account-page.component';
+import { AdminPanelPageComponent } from './admin-panel-page/admin-panel-page.component';
 
 export const authRoutes: Routes = [
   {
     path: 'auth/login',
     component: LoginPageComponent,
+  },
+  {
+    path: 'my-account',
+    component: MyAccountPageComponent,
+  },
+  {
+    path: 'admin-panel',
+    component: AdminPanelPageComponent,
   },
 ];
